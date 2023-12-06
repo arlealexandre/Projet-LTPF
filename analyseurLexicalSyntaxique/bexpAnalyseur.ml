edb7d16cdcf69de58dde9dbd6be5ed65f31d06c1
Emin Gundogan <emin.gundogan@etu.univ-grenoble-alpes.fr>
2023-12-06 10:41:58 +0100

    ranalist début

--- analyseurLexicalSyntaxique/bexpAnalyseur.ml
+++ analyseurLexicalSyntaxique/bexpAnalyseur.ml
@@ -73,17 +73,14 @@ let isDigit = fun c ->
 let p_letter : char analist = terminal_cond (isLetter)
 let p_digit : char analist = terminal_cond (isDigit)
 
-let p_suite : char analist = fun l ->
-  l |> p_letter -| p_digit
+let rec p_suite : char analist = fun l ->
+  l |> ((p_letter -| p_digit) --> p_suite) -| epsilon
 
 let p_variable : char analist = fun l ->
-  l |> p_letter --> (p_suite -| epsilon)
+  l |> p_letter --> p_suite
 
 let rec p_nombre : char analist = fun l ->
   l |> p_digit --> (p_nombre -| epsilon)
-
-let p_valeur : char analist = fun l ->
-  l |> p_aexp -| p_bool
 
 let rec p_aexp : char analist = fun l ->
   l |> p_plus and
@@ -97,6 +94,9 @@ let rec p_aexp : char analist = fun l ->
       l |> (terminal '*' --> p_number --> p_mul_opt) -| epsilon and
     p_number : char analist = fun l ->
       l |> p_nombre -| (terminal '(' --> p_aexp --> terminal ')')
+
+let p_valeur : char analist = fun l ->
+  l |> p_aexp -| p_bool
   
 
 let rec p_programme : char analist = fun l ->
@@ -124,4 +124,57 @@ let rec p_programme : char analist = fun l ->
 let _ = isLetter 'a'
 let _ = p_aexp (list_of_string ("5+3"))
 let _ = p_declaration (list_of_string ("inta:=5+3"))
-let _ = p_programme (list_of_string ("inta:=true;a:=12+13*5;if(true){boola:=2}elseif(false){}else{}"))
+let _ = p_if (list_of_string ("if(false){}"))
+let _ = p_programme (list_of_string ("inta:=true;aaa:=12+13*5;if(true){boola:=2}elseif(false){}else{}"))
+
+type typeV =
+  | Int
+  | Bool
+  | Fun
+
+type programme =
+  | Skip
+  | Seq of programme * programme
+  | AffectI of char list * int
+  | AffectB of char list * bool
+  | AffectF of char list * programme
+  | Declaration of typeV * programme
+  | If of bexp * programme * programme
+  | While of bexp * programme
+
+type dictionnaire =
+  | VInt of typeV * int
+  | VBool of typeV * bool
+  | VFun of typeV * programme
+
+let rec concat = fun (l1 : char list) (l2 : char list) ->
+  match l1 with
+  | x::q -> x::(concat q l2)
+  | _ -> l2
+
+let isLetterR = fun c ->
+  if((Char.code c > 64 && Char.code c < 91) || (Char.code c > 96 && Char.code c < 123))
+  then Some(c) else None
+
+let isDigitR = fun c ->
+  let x = Char.code c - Char.code '0' in if(x >= 0 && x <= 9) then Some(c) else None
+
+let _ = isDigitR '8'
+let _ = let x,q = (pr_digit (list_of_string "8")) in let y,q' = (pr_letter (list_of_string "a")) in  concat [x] [y]
+
+let pr_letter : (char, char) ranalist = terminal_res (isLetterR)
+let pr_digit : (char, char) ranalist = terminal_res (isDigitR)
+
+let rec pr_suite : (char list, char) ranalist = fun (x : char list) (l : char list) ->
+  l |> (pr_letter ++> fun (x' : char)  -> pr_suite (concat x [x']))
+(***+| (pr_digit ++> fun i -> pr_suite (concat x [i])) +| epsilon_res x***)
+
+let pr_nom : (char list, char) ranalist = fun l ->
+  l |> pr_letter ++> fun x -> pr_suite [x]
+
+let rec pr_programme (programme, char) ranalist = fun l ->
+  l |> pr_instruction ++> fun exp -> pr_separateur exp and
+    pr_declaration : (programme, char) = fun l ->
+      l |> (t_int -+> pr_affectation ++> fun exp -> epsilon_res (Declaration (Int, exp))) +| (t_bool -+> pr_affectation ++> fun exp -> epsilon_res (Declaration (Bool, exp))) +| (t_fun -+> pr_affectation ++> fun exp -> epsilon_res (Declaration (Fun, exp))) and
+    pr_affectation : (programme, char) = fun l ->
+      l |> pr_nom ++> fun nom -> terminal ':' --> terminal '=' -+> pr_valeur
